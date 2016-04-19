f6af68d180d9d8bd8f595cdac4aac5db2cf932e5
JARRAD THOMAS <jarrad@nike.cs.uga.edu>
2016-04-19 13:58:32 -0400

    Initial commit

--- src/main/java/cs1302/calc/RecursiveMath.java
+++ src/main/java/cs1302/calc/RecursiveMath.java
@@ -10,37 +10,70 @@ package cs1302.calc;
  * @author AUTHOR
  */
 public class RecursiveMath implements Math {
-
+	
     public int inc(int n) {
-        throw new UnsupportedOperationException();
+      return n+1;
     } // inc
-
+	
     public int dec(int n) {
-        throw new UnsupportedOperationException();
+       return n-1;
     } // dec
 
-    public int add(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+    public int add(int lhs, int rhs) { // lhs or rhs cannot sum up to something more than 2200000000. Why?
+    	if(rhs ==0){return lhs;}
+    	return add(succ(lhs),pred(rhs));
+        
     } // add
+    
 
     public int sub(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        if(rhs ==0){return lhs;}
+        if(lhs ==0){return 0;}
+        else{return sub(pred(lhs),pred(rhs));}
     } // sub
 
     public int mul(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+    	return mul_acc(lhs,lhs,rhs);
     } // mul
 
+    public int mul_acc(int sum,int lhs,int rhs){
+    	if(rhs==0){return 0;}
+    	if(rhs==1){return sum;}
+    	else{return mul_acc(add(sum,lhs),lhs,pred(rhs));}
+    }
+    
     public int div(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        if(lhs < rhs){return 0;}
+        if(lhs == rhs) {return 1;}
+        else{return succ(div(sub(lhs,rhs),rhs));}
     } // div
 
     public int fac(int n) {
-        throw new UnsupportedOperationException();
+        return fac_acc(1,n);
     } // fac
-
-    public int pow(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+    
+    public int fac_acc(int acc, int n){
+    	if(n==1 || n == 0)return acc; // modified the base case to handle 1! and 0!.
+    	return fac_acc(mul(acc,n),pred(n));
+    }
+    
+    public int pow(int lhs, int rhs){
+    	return pow_acc(lhs,lhs,rhs);
+    }
+    public int pow_acc(int acc, int n, int m) { //idk how i did it, but I did. [Analyze This]
+        if( m==1) return acc;
+        return pow_acc(mul(acc,n),n,pred(m));
     } // pow
+    
 
+  
+public static void main( String[] args){
+RecursiveMath r = new RecursiveMath();
+System.out.println(r.mul(9, 1));
+System.out.println(r.sub(10000, 1000));
+System.out.println(r.div(9000, 1000));
+System.out.println(r.fac(0));
+System.out.println(r.pow(4, 4));
+//System.out.println(r.mul(200000, 1000));// not accepted.
+}
 } // RecursiveMath
