ef056fbd10b5d53b72381f660ab7538898d49c05
Michaeljurado42 <maj64642@uga.edu>
2016-04-24 00:44:58 -0400

    Finally fixed the number input

--- src/main/java/cs1302/calc/EqualsButton.java
+++ src/main/java/cs1302/calc/EqualsButton.java
@@ -15,12 +15,40 @@ import javafx.beans.property.SimpleBooleanProperty;
 
 public class EqualsButton extends Button{
 	
+	public String getInfixText(String text){
+
+		String infix = ""; //will return this
+
+		int tLength = text.length();
+
+		String temp ="";
+		int i = 0; 
+	//	while(i< tLength){
+		int charValue = 0;
+		String newChar = "";
+		charValue = 50;
+		temp = "";
+		while  (i< tLength) {
+				charValue = text.charAt(i);
+				if ( (charValue <= 57) && (charValue >= 48) && (i< tLength) ){
+					infix += text.substring(i, i+1);
+				}
+				else{
+					infix += " " + text.substring(i, i+1) + " ";
+				}
+				i++;
+		}
+		return infix;		
+	}
 
 	public EqualsButton(Label input, Label output,SimpleBooleanProperty useRecursion){
 		super("="); /* Calls the constructor of the Button parent class and gives it a String input to label the button with. */
 		this.setOnAction( event -> {
 			/*Converts inputLabel to StringList*/
-			String[] infix = input.getText().split(""); //thank god this works
+			String infixString = this.getInfixText(input.getText());
+			System.out.println("Infec string" + infixString);
+
+			String[] infix = infixString.split(" "); //thank god this works
 			String[] postFix = ReversePolishNotation.infixToPostfix(infix);
 			// for(String i: infix){
 			// 	System.out.println("infx"+infix);
