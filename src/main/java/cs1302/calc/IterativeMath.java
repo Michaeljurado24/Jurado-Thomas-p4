79fa9f867b5098437e0b9df6883cec0a63701545
JarradLamar <JLT44434@uga.edu>
2016-04-24 18:55:55 -0400

    Java Docs for Iterative and RecursiveMath

--- src/main/java/cs1302/calc/IterativeMath.java
+++ src/main/java/cs1302/calc/IterativeMath.java
@@ -1,24 +1,45 @@
 package cs1302.calc;
 
-/* Implement the Math intere using iteration here. For each method, replace
- * the throw statement with your implementation.
- */
+
 
 /**
- * REPLACE ME WITH ACTUAL JAVADOC FOR THIS CLASS.
+ * 
  *
- * @author AUTHOR
+ * @author Michael Jurado and Jarrad Thomas
  */
 public class IterativeMath implements Math {
 
+	/**
+	 * 
+	 * Takes in the int n and increments it by one. This incremented value is returned.
+	 * @param int n
+	 * @return int
+	 */
     public int inc(int n) {
      return n+1;
     } // inc
 
+	/**
+	 * 
+	 * Takes in the int n and decrements it by one. This decremented value is returned.
+	 * @param int n
+	 * @return int
+	 */
     public int dec(int n) {
         return n-1;
     } // dec
-
+    
+    /**
+     * 
+     * This method uses the inc() and dec() methods. The method takes the int value lhs and increments it by 1 for the 
+     * number of times indicated by the int value rhs. A secondary int called sum is declared. The variable sum is used in a while loop to store
+     * each incrementation of lhs. Each time sum is incremented the value of rhs is decresed by 1 until rhs reaches 0. When rhs = 0 the loop
+     * is broken and sum is returned.
+     * 
+     * @param int lhs
+     * @param int rhs
+     * @return int
+     */
     public int add(int lhs, int rhs) {
     	int sum = lhs;
     	if(rhs == 0) return sum;
@@ -29,6 +50,17 @@ public class IterativeMath implements Math {
     	return sum;		
     } // add
 
+    /**
+     * 
+     * This method takes the int value lhs and decreases it by 1 a number of times equal to the int value rhs.
+     * The function stores the value of lhs in a secondary variable called dif. if rhs is equal to zero dif is returned by the method
+     * If rhs is greater than zero diff and rhs are decremented by 1 repeatedly in a while loop till rhs is equal to 1.
+     * When rhs is equal to 1 the loop breaks and dif is returned.
+     * 
+     * @param int lhs
+     * @param int hs
+     * @return int
+     */
     public int sub(int lhs, int rhs) {
         int dif = lhs;
         if(rhs ==0) return dif;
@@ -38,7 +70,20 @@ public class IterativeMath implements Math {
         }
         return dif;
     } // sub
-
+    
+    /**
+     * 
+     * This method takes the the int value lhs  and repeatedly adds sets of 1's to lhs. 
+     * The length of each set of 1's is equal to the int value rhs. the initial value of lhs is saved to
+     * another variable called product. Product is then used for the rest of the method. Using a while loop the 
+     * value of product is incremented by some set of 1's with length equal to rhs. Each change to product also leads
+     * to a substraction in the value of rhs by one. Eventually rhs will equal less than one. Once this happens the while-loop
+     * breaks and the int product is returned by the method.
+     * 
+     * @param int lhs
+     * @param int rhs
+     * @return int
+     */
     public int mul(int lhs, int rhs) {
         int product =lhs;
         if( (rhs ==0) || (lhs == 0) ) return 0;
@@ -50,7 +95,17 @@ public class IterativeMath implements Math {
         }
         return product;
     } // mul
-
+    
+    /**
+     * 
+     * The div function takes a int lhs and finds out how many times the value rhs can be subtracted from lhs.
+     * If rhs is equal to 0 the function returns a int equal to 0. If rhs is greater than 0 the method will subtract
+     * rhs from lhs and incremented quotient by 1. When lhs reaches a value of one the loop breaks and the int quotient is returned.
+     * 
+     * @param int lhs
+     * @param int rhs
+     * @return int
+     */
     public int div(int lhs, int rhs) {
         int quotient = 0;
         if(rhs ==0) return 0;
@@ -60,8 +115,20 @@ public class IterativeMath implements Math {
         }
         return quotient;
     } // div
-
-    public int fac(int n) {// not working.
+    
+    /**
+     * 
+     * This function takes a int n and returns the factorial product of this number. If n is equal to 0 or 1 the 
+     * method will return a value of 1. As long as n is greater than the previously mentioned whole numbers a while-loop
+     * will be initiated. Within this loop product is contanstly multiplied by n. Each iteration of the loop n is subtracted by 1 using the
+     * subtration method. When the while-loop finally ends product will be equal to n*(n-1)*(n-2)*(n-3)... and so on.
+     * This product is returned by the method.
+     * 
+     * @param int lhs
+     * @param int rhs
+     * @return int
+     */
+    public int fac(int n) {
     	  int product =1;
         if( n == 0 || n ==1) return product;
         while(n >=1){
@@ -70,7 +137,20 @@ public class IterativeMath implements Math {
         }
         return product;
     } // fac
-
+    
+    /**
+     * 
+     * This method takes the value lhs and multiplies itself by its own value a number of times equal to the value rhs.
+     * The method initiates a int called sum, set's it equal to 1. Sum is used to store each iteration of lhs times itself.
+     * If rhs is equal to 0 the method returns a value of 1, because anything to the power of zero is 1. As long as rhs 
+     * is greater than 0, the method will activate a while loop. That loop subtracts rhs by 1 and then multiplies lhs times itself.
+     * The loop of the while-loop stores the result of lhs*1 which is equal to lhs. Every other iteration of the loop has sum multiplying lhs times lhs.
+     * The loop breaks when rhs finally decrements to 1 and the int sum is returned.
+     * 
+     * @param int lhs
+     * @param int rhs
+     * @return int
+     */
     public int pow(int lhs, int rhs) {
         int sum = 1;
         if (rhs == 0) { return 1; }
@@ -80,16 +160,5 @@ public class IterativeMath implements Math {
         }
         return sum;
     } // pow
-
-
-    @Override
-    public int lshift(int lhs, int rhs) {
-        return mul(lhs, pow(2, rhs));
-    } // lshift
-
-@Override     
-    public int rshift(int lhs, int rhs) {
-        return div(lhs, pow(2, rhs));
-    } // rshift
 
 } // IterativeMath
