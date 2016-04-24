40bf3641d6313d18431f7800c37394e370be37b5
JarradLamar <JLT44434@uga.edu>
2016-04-24 19:55:45 -0400

    blue

--- src/main/java/cs1302/calc/BinaryPane.java
+++ src/main/java/cs1302/calc/BinaryPane.java
@@ -4,7 +4,7 @@ import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
 import javafx.geometry.Insets;
 import javafx.scene.layout.VBox;
-
+import javafx.scene.paint.Color;
 import javafx.beans.property.SimpleStringProperty;
 
 /*A wise man once said: Use harms and even destroys beauty. The noblest function of an object is to be contemplated.
@@ -52,22 +52,28 @@ public class BinaryPane  extends FlowPane{
 			
 		} //first while
 		Label label1 = new Label("63");
+		label1.setTextFill(Color.BLUE);
 		boxList[0].getChildren().add(label1);
 
 		Label label2 = new Label("47");
 		boxList[4].getChildren().add(label2);
-
-		Label label3 = new Label("   32");
+		label2.setTextFill(Color.BLUE);
+		
+		Label label3 = new Label("    32");
+		label3.setTextFill(Color.BLUE);
 		boxList[7].getChildren().add(label3);
 
 		Label label4 = new Label("31");
 		boxList[8].getChildren().add(label4);
+		label4.setTextFill(Color.BLUE);
 
 		Label label5 = new Label("15");
 		boxList[12].getChildren().add(label5);
+		label5.setTextFill(Color.BLUE);
 
-		Label label6 = new Label("0");
+		Label label6 = new Label("     0");
 		boxList[15].getChildren().add(label6);
+		label6.setTextFill(Color.BLUE);
 
 		this.getChildren().clear();
 		for(VBox box: boxList){
@@ -79,7 +85,7 @@ public class BinaryPane  extends FlowPane{
 
 	public void setFormat(){
 		this.setVgap(2);
-		this.setHgap(13);
+		this.setHgap(25);
 		this.setPadding(new Insets(10,10,10,10) );
 		this.setPrefWrapLength(WIDTH);		
 	}
